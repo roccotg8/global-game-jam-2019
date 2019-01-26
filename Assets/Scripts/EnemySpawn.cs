226fe2a62decfdab4e39e7e7518ef64954e6c077
Adela <at324@njit.edu>
2019-01-26 17:07:20 -0500

    base timer added, enemy behavior script started

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    // script periodicaly spawns an enemy entity relative to player location and based on time passed 
+    // and if the player is colliding with a particular game object
+
+    public GameObject enemy;  //enemy prefab
+    bool spawn = false;  // dont spawn on default
+    GameObject player;  // player 
+    public Timer time;  // time count
+    float speed; // how fast the enemy moves
+    float spawnTime = 2f; //time til spawn 
+    Vector2 spawnPos; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player");  //tracks the player object
+        spawnPos = player.transform.position; 
+        speed = 5f;
+        spawnTime = 2f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Hunt(); 
+    }
+
+    void Spawn ()
+    {
+        Instantiate(enemy );
+    }
+
+    void Hunt()
+    {
+        //lerp the enemy towards the player 
+        
+    }
+}
+
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float count = 300f;
+    public Text clock;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count -= Time.deltaTime;
+
+
+        string minutes = ((int)count / 60).ToString();
+        string seconds = (count % 60).ToString("f2");
+
+        clock.text = minutes + ":" + seconds;
+    }
+}
