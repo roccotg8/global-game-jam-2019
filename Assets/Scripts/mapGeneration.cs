1068cab2dfe2ca2fb779ff90ecfd120b968e0276
roccotg8 <rtg8@njit.edu>
2019-01-26 00:44:11 -0500

    generation of map using basic tiles

---
+++ Assets/Scripts/mapGeneration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapGeneration : MonoBehaviour
+{
+
+    [SerializeField]
+    int width, height;
+
+    [SerializeField]
+    public GameObject[] groundTiles;
+
+    [SerializeField]
+    public int offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                int tile = Random.Range(0, groundTiles.Length);
+                Vector2 pos = new Vector2(x*offset, y*offset);
+                Instantiate(groundTiles[tile], pos, Quaternion.identity);
+            }
+        }
+    }
+}
