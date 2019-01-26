a9732516a33579c185b59227756fbff7563777e1
roccotg8 <rtg8@njit.edu>
2019-01-26 05:55:15 -0500

    added random locations for spawn and one secret room. cleaned up the code and added place holders for spawn locations and the secret room

--- Assets/Scripts/mapGeneration.cs
+++ Assets/Scripts/mapGeneration.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class mapGeneration : MonoBehaviour
 {
-
     [SerializeField]
-    int width, height, minGroundPasses, maxGroundPasses, groundObjectPercentChance;
+    int width, height, minGroundPasses, maxGroundPasses, groundObjectPercentChance, spawnPercentChance, roomChance;
 
     [SerializeField]
     float terrainOffset;
 
     [SerializeField]
-    GameObject[] terrainTiles, groundObjects;
+    GameObject[] terrainTiles, groundObjects, spawnTile, secretRoom;
 
     [SerializeField]
     GameObject hubTile;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         GenerateTerrain();
+        GenerateGroundObjects();
+        GenerateSpawnTiles();
+        GenerateSecretRoom();
+        PlaceHub();
     }
 
     void GenerateTerrain()
@@ -28,39 +30,67 @@ public class mapGeneration : MonoBehaviour
         //create the base terrain
         for (int x = 0; x < width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
                 int tile = Random.Range(0, terrainTiles.Length);
-                Vector2 pos = new Vector2(x*terrainOffset, y*terrainOffset);
+                Vector2 pos = new Vector2(x * terrainOffset, y * terrainOffset);
                 Instantiate(terrainTiles[tile], pos, Quaternion.identity);
             }
         }
+    }
 
+    void GenerateGroundObjects()
+    {
         //generate the random stuff on the ground
         int groundPasses = Random.Range(minGroundPasses, maxGroundPasses);
         for(int i = 0; i < groundPasses; i++)   //do each ground pass
         {
-            for (int x = 0; x < width; x++) //iterate through x values of the map
+            randomInTilePlacement(groundObjects, groundObjectPercentChance);           
+        }
+    }
+
+    void GenerateSpawnTiles()
+    {
+        //generate spawn locations
+        randomInTilePlacement(spawnTile, spawnPercentChance);
+    }
+
+    void GenerateSecretRoom()
+    {
+        //generate secret room
+        randomInTilePlacement(secretRoom, roomChance, true);
+    }
+
+    void PlaceHub()
+    {
+        //place the hub in the center of the map
+        Vector2 hubPos = new Vector2(((width / 2.0f) * terrainOffset) - (terrainOffset / 2.0f), ((height / 2.0f) * terrainOffset) - (terrainOffset / 2.0f));
+        Instantiate(hubTile, hubPos, Quaternion.identity);
+    }
+
+    void randomInTilePlacement(GameObject[] objects, int percentChance, bool isRoom = false)
+    {
+        for (int x = 0; x < width; x++) //iterate through x values of the map
+        {
+            for (int y = 0; y < height; y++)    //iterate through y values of the map
             {
-                for (int y = 0; y < height; y++)    //iterate through y values of the map
+                if (Random.Range(1, 100) <= percentChance)   //% chance of having ground stuff
                 {
-                    if (Random.Range(1,100) >= groundObjectPercentChance)   //50% chance of having ground stuff
+                    int tile = Random.Range(0, objects.Length);    //randomly select the ground stuff tile
+
+                    float inTileOffsetX = ((Random.Range(-500, 500) / 1000.0f) * terrainOffset);  //randomly chooses x offset
+                    float inTileOffsetY = ((Random.Range(-500, 500) / 1000.0f) * terrainOffset);  //randomly chooses y offset
+
+                    Vector2 pos = new Vector2((x * terrainOffset) + inTileOffsetX, (y * terrainOffset) + inTileOffsetY);  //sets position of ground object
+
+                    Instantiate(objects[tile], pos, Quaternion.identity);
+
+                    if(isRoom)
                     {
-                        int tile = Random.Range(0, groundObjects.Length);    //randomly select the ground stuff tile
-
-                        float inTileOffsetX = ((Random.Range(-500,500)/1000.0f) * terrainOffset);  //randomly chooses x offset
-                        float inTileOffsetY = ((Random.Range(-500,500)/1000.0f) * terrainOffset);  //randomly chooses y offset
-
-                        Vector2 pos = new Vector2((x*terrainOffset)+inTileOffsetX, (y*terrainOffset)+inTileOffsetY);  //sets position of ground object
-                        
-                        Instantiate(groundObjects[tile], pos, Quaternion.identity);
+                        return;
                     }
                 }
-            }           
+            }
         }
-
-        //place the hub in the center of the map
-        Vector2 hubPos = new Vector2(((width/2.0f)*terrainOffset)-(terrainOffset/2.0f), ((height/2.0f)*terrainOffset)-(terrainOffset/2.0f));
-        Instantiate(hubTile, hubPos, Quaternion.identity);
     }
 }
